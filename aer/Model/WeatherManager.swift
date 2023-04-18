913fe6babfdede7df2076ec0fa65fc76b1be4fdf
Ata <atakanozalan@gmail.com>
2023-04-18 17:49:10 +0300

    using the URLSession for networking

--- aer/Model/WeatherManager.swift
+++ aer/Model/WeatherManager.swift
@@ -17,8 +17,29 @@ struct WeatherManager{
     
     func performRequest(urlString: String){
         //1. Create a URL
-        //2. Create a URLSession
-        //3. Give the session a task
-        //4. Start the task
+        
+        if let url = URL(string: urlString){
+            //2. Create a URLSession
+            
+            let session = URLSession(configuration: .default)
+            
+            //3. Give the session a task
+            
+            let task =  session.dataTask(with: url, completionHandler: handle(data: response: error: ))
+            
+            
+            //4. Start the task
+            task.resume()
+        }
+    }
+    func handle(data: Data?, response: URLResponse?, error: Error?){
+        if error != nil{
+            print(error!)
+            return
+        }
+        if let safeData = data{
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
     }
 }
