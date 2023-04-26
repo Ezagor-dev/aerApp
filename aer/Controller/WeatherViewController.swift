303690df1e3abaefc7064d2d16321ebc04ec7517
Ata <atakanozalan@gmail.com>
2023-04-26 20:00:54 +0300

    UI Label Changing

--- aer/Controller/WeatherViewController.swift
+++ aer/Controller/WeatherViewController.swift
@@ -53,7 +53,12 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
     }
     
     func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel){
-        print(weather.temperature)
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            self.cityLabel.text = weather.cityName
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+        }
+        
     }
     
     func didFailWithError(error: Error) {
