397f6df8d3126e488c17f209b212c0527db5929f
Ata <atakanozalan@gmail.com>
2023-04-26 14:53:07 +0300

    JSON Decoder

---
+++ aer/Model/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  aer
+//
+//  Created by Ezagor on 26.04.2023.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Decodable {
+    let temp: Double
+}
+
+struct Weather: Decodable {
+    let description: String
+}
+
