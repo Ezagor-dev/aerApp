ef9f005bc83f8fb8238ddcd0b3a736ccd49f99b7
Ata <atakanozalan@gmail.com>
2023-04-28 13:48:02 +0300

    taking latitude and longitude informations from the user

--- aer/Controller/WeatherViewController.swift
+++ aer/Controller/WeatherViewController.swift
@@ -89,7 +89,12 @@ extension WeatherViewController: WeatherManagerDelegate{
 
 extension WeatherViewController: CLLocationManagerDelegate{
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        print("Got location data")
+        if let location = locations.last{
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            print(lat)
+            print(lon)
+        }
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
